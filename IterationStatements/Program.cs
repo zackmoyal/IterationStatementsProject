217288ea7b03312a0418321d5632abc570f3d7d8
Zack Moyal <zack.moyal@gmail.com>
2024-01-03 01:48:13 -0800

    corrected line 70

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -67,7 +67,7 @@ namespace IterationStatements
                 // AND as long as i is greater than or equal to 0
                 // Decrement i by 1
 
-            for (int i = 199; i = 0; i--) //start for loop here
+            for (int i = 199; i >= 0; i--) //start for loop here
             {
                 // place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine($"{numbers[i]} ");
